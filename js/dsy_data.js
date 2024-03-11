9a4366653d5ad0820f9bcb50159ce3534aa7227a
shareBeautiful <1454167941@qq.com>
2024-03-11 18:00:33 +0800

    Add files via upload

---
+++ js/dsy_data.js
@@ -0,0 +1,177 @@
+// 页面数据
+// 默认显示语言 中文：zh,英文：en
+let lang = 'en'
+
+let dsy_data = {
+    // 图片目录
+    imgSrc: 'image/',
+
+    // logo 旁名字
+    title: 'Dsy Tech Co.Ltd',
+
+    // 导航文字
+    menus: [
+        {
+            id: 'a',
+            desc: 'Email'
+        },
+        {
+            id: 'b',
+            desc: 'Linkedin',
+        }
+    ],
+
+    // 模块名字
+    productTitle: {
+        productShow: 'Product Show',
+        productDetail: 'Product Details',
+        aboutUs: 'About Us',
+        cultureData: 'Culture',
+        contactUs: 'Contact Us'
+    },
+
+    // banner轮播
+    banner: [
+        {
+            img: 'banner_1.png',
+            desc: 'Park lounge',
+        },
+        {
+            img: 'banner_2.png',
+            desc: 'Park lounge',
+        },
+        {
+            img: 'banner_3.png',
+            desc: 'Park lounge',
+        },
+        {
+            img: 'banner_4.png',
+            desc: 'Park lounge',
+        },
+        {
+            img: 'banner_5.png',
+            desc: 'Park lounge',
+        },
+        {
+            img: 'banner_6.png',
+            desc: 'Park lounge',
+        }
+    ],
+
+
+    // 产品与服务
+    productShow: [
+        {
+            img: 'product_5.png',
+            desc: 'Park lounge',
+            info: ''
+        },
+        {
+            img: 'product_6.png',
+            desc: 'Pavilion swing',
+            info: ''
+        },
+        {
+            img: 'product_7.png',
+            desc: 'Rocking chair',
+            info: ''
+        },
+        {
+            img: 'product_8.png',
+            desc: 'Vehicle tents',
+            info: ''
+        },
+        {
+            img: 'product_9.png',
+            desc: 'Roman umbrella',
+            info: ''
+        },
+        {
+            img: 'product_10.png',
+            desc: 'Cast aluminum tables and chairs',
+            info: ''
+        },
+        /*{
+            img: 'product_1.jpg',
+            desc: 'Premium quality',
+            info: '10cm aluminum umbrella pole.\n' +
+                'Umbrella pole wall thickness 3.2mm\n' +
+                'Umbrella rib material. Aluminum alloy frame.'
+        },
+        {
+            img: 'product_2.jpg',
+            desc: 'Built-in LED light',
+            info: '10cm aluminum umbrella poleUmbrella wall thickness 2.48mmUmbrella rib material. carbon steel frame with LED lightGuaranteed not to fade within 5 years'
+        },
+        {
+            img: 'product_3.jpg',
+            desc: '',
+            info: '10cm aluminum umbrella pole\n' +
+                'Umbrella pole wall thickness 2.48mm Umbrella rib material thickened carbon\n' +
+                'steel frame\n' +
+                'Guaranteed not to fade within 5 years'
+        },
+        {
+            img: 'product_4.jpg',
+            desc: '',
+            info: '7.5cm aluminum umbrella pole.Umbrella pole wal thickness 2.2mm Umbrella rib material: carbon steel frame\n' +
+                'Guaranteed not to fade within 2 years'
+        }*/
+    ],
+
+    // 产品详情
+    productDetail: [
+        {
+            img: 'product_1.jpg',
+            desc: '10cm aluminum umbrella poleUmbrella pole wall thickness 3.2mm Umbrella rib material. Aluminum alloy frame.'
+        },
+        {
+            img: 'product_2.jpg',
+            desc: '10cm aluminum umbrella poleUmbrella wall thickness 2.48mm Umbrella rib material. carbon steel frame with LED lightGuaranteed not to fade within 5 years'
+        },
+        {
+            img: 'product_3.jpg',
+            desc: '10cm aluminum umbrella poleUmbrella pole wall thickness 2.48mm Umbrella rib material thickened carbon steel frame' +
+                'Guaranteed not to fade within 5 years'
+        },
+        {
+            img: 'product_4.jpg',
+            desc: '7.5cm aluminum umbrella poleUmbrella pole wal thickness 2.2mm Umbrella rib material: carbon steel frame\n' +
+                'Guaranteed not to fade within 2 years'
+        }
+        /*{
+            img: 'product_detail_01.png',
+            desc: 'Fabric material',
+        },
+        {
+            img: 'product_detail_02.png',
+            desc: 'Cast aluminum handle'
+        },
+        {
+            img: 'product_detail_03.png',
+            desc: 'Water tank water injection and sand injection display'
+        },
+        {
+            img: 'product_detail_04.png',
+            desc: 'More details about Roman umbrellas'
+        }*/
+    ],
+
+    // 关于我们
+    aboutUs: [
+        'DSY tech Co. Ltd, located in a coastal city, Shenzhen in China. We specialize in the production and sales of outdoor furniture production, with a plant area of 30,000 square meters, our outdoor furniture products cover outdoor shade, outdoor tables and chairs, courtyard facilities, pavilion swings. Until now, we have collected a wealth inexperience of production, servicing for tens of thousands clients.'
+    ],
+
+    // 企业文化
+    cultureData: [],
+
+    // 联系我们
+    contactUs: {
+        qrCode: 'qr.png',
+        tel: '15626121215',
+        address: 'Address: Shenzhen, China',
+        wechat: 'Tel(Wechat)：+86 15626121215',
+        whatsapp: 'WhatsApp: +852 92976978',
+        email: 'mayuan57225@163.com'
+    }
+}
